e426f31853fc3e1e1c30332e3ac5cf2f713dc053
Ruanziin <ruanpeixotorip@gmail.com>
2025-05-21 22:42:01 -0300

    Adicionando lista 03

--- 03-funcoes/exercicios/04-tipo-nadador.c
+++ 03-funcoes/exercicios/04-tipo-nadador.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
-int main(){
+char catNadador(int id){
+    if(id >= 5 && id <= 7){
+        return 'A';
+    } else if(id >= 8 && id <= 10){
+        return 'B';
+    } else if(id >= 11 && id <= 13){
+        return 'C';
+    } else if(id >= 14 && id <= 17){
+        return 'D';
+    } else {
+        return 'E';
+    }
+}
 
-    
+int main() {
+
+    int idade, categoria;
+    printf("Digite a idade: ");
+    scanf("%d", &idade);
+
+    categoria = catNadador(idade);
+
+    printf("Categoria do nadador: %c\n", categoria);
 
     return 0;
 }
--- 03-funcoes/exercicios/05-duracao-jogo.c
+++ 03-funcoes/exercicios/05-duracao-jogo.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
-int main(){
+//Faça uma função que recebe, por parâmetro, a hora de início e a hora de término de um
+//jogo, ambas subdivididas em 2 valores distintos: horas e minutos. A função deve retornar,
+//também por parâmetro, a duração do jogo em minutos, considerando que o tempo máximo
+//de duração de um jogo é de 24 horas e que o jogo pode começar em um dia e terminar no
+//outro.
 
-    
+int calcularDuracao(int hInicio, int mInicio, int hFim, int mFim) {
+    int inicio = hInicio * 60 + mInicio;
+    int fim = hFim * 60 + mFim;
+    int duracao;
+
+    if (fim >= inicio) {
+        duracao = fim - inicio;
+    } else {
+        duracao = (24 * 60 - inicio) + fim;
+    }
+
+    return duracao;
+}
+
+int main() {
+    int horaInicio, minInicio, horaFim, minFim;
+    int duracao;
+
+    printf("Digite a hora e minuto de início (hh mm): ");
+    scanf("%d %d", &horaInicio, &minInicio);
+
+    printf("Digite a hora e minuto de fim (hh mm): ");
+    scanf("%d %d", &horaFim, &minFim);
+
+    duracao = calcularDuracao(horaInicio, minInicio, horaFim, minFim);
+
+    printf("Duração do jogo: %d minutos\n", duracao);
 
     return 0;
 }
